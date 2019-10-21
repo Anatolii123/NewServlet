e58f6d4c80c95d62352ef237be345acfe3806b85
AKusakin <anatolii.kusakin1234@gmail.com>
2019-10-21 15:29:02 +0500

    NewSpring2Commit

--- src/main/java/application/config/SpringConfig.java
+++ src/main/java/application/config/SpringConfig.java
@@ -1,7 +1,9 @@
 package application.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"application.service","application.dao"})
 public class SpringConfig {
 }
